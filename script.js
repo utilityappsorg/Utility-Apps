a0858c5aeeec2d943c23c6476cbb84faf76418a9
Nathanfeumy <Nathanfeumy@gmail.com>
2025-07-15 11:15:26 -0500

    worked on the app

--- script.js
+++ script.js
@@ -28,10 +28,4 @@ document.addEventListener('DOMContentLoaded', function() {
         `;
     };
 });
-document.querySelectorAll('.sidebar a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const appUrl = this.getAttribute('data-app');
-    document.getElementById('app-frame').src = appUrl;
-  });
-});
+
